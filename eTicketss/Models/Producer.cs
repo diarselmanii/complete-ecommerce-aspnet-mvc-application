05c9826d55be9bc7c651b084a1201fe5bd235fa7
Diar Selmani <selmanidiar2@gmail.com>
2023-05-05 00:11:04 +0200

    Changes

---
+++ eTicketss/Models/Producer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicketss.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //RELATIONSHIPS
+
+        public List<Movie> Movies { get; set; }
+    }
+}
